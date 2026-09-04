5b5ad067682a933b576b352e72e4c05fd4a2f0ae
Nav <arnavnemade1@gmail.com>
2026-09-04 17:18:53 +0000

    Updating Project python file

--- project5.py
+++ project5.py
@@ -1,1 +1,23 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pylot as plt
+from scipy.sparse.linalg import svd
+
+M = pd.read_csv('co_occur.csv', header=None).values
+
+m_tilde = np.log1p(M)
+
+U, S, Vt = svd(m_tilde, k=100)
+
+np.savez('svd_top100.npz',U=U, S=S, Vt=Vt)
+
+plt.figure(figsize=(8,5))
+plt.plot(range(1,101),S, linewidth=2)
+plt.title('Top 100 Singular Values')
+plt.xlabel('Index $i$')
+plt.ylabel('Singular Value Index')
+plt.grid(True)
+plt.tight_layout()
+plt.savefig('singular_values_plot.png')
+plt.show
 
