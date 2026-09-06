a04e655d0cf267ebdd99d9508a73a60a5d3fba83
Arnav Nemade <arnavnemade1@gmail.com>
2026-09-06 16:39:52 -0500

    Fix merge conflicts

--- project5.py
+++ project5.py
@@ -1,13 +1,13 @@
 import numpy as np 
 import pandas as pd 
-import matplotlib.pylot as plt
-from scipy.sparse.linalg import svd
+import matplotlib.pyplot as plt
+from scipy.sparse.linalg import svds
 
 M = pd.read_csv('co_occur.csv', header=None).values
 
 m_tilde = np.log1p(M)
 
-U, S, Vt = svd(m_tilde, k=100)
+U, S, Vt = svds(m_tilde, k=100)
 
 np.savez('svd_top100.npz',U=U, S=S, Vt=Vt)
 
@@ -19,5 +19,4 @@ plt.ylabel('Singular Value Index')
 plt.grid(True)
 plt.tight_layout()
 plt.savefig('singular_values_plot.png')
-plt.show
-
+plt.show()
